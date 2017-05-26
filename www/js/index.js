1237326be78e8b7b8aff8a2bb7fc0c62dad6d75f
tuebom <tuebom.wirya@gmail.com>
2017-05-26 19:11:57 +0800

    Update

--- www/js/index.js
+++ www/js/index.js
@@ -36,6 +36,10 @@ var app = {
        
         // place our admob ad unit id here
         var admobid = {};
+
+        var idleTimer = null;
+        var idleState = false;
+        var idleWait = 10000;
  
         if( /(android)/i.test(navigator.userAgent) ) {
           admobid = { // for Android
@@ -57,7 +61,7 @@ var app = {
         if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
        
         // this will create a banner on startup
-        AdMob.createBanner( {
+        /*AdMob.createBanner( {
           adId: admobid.banner,
           adSize: 'SMART_BANNER',
           position: AdMob.AD_POSITION.BOTTOM_CENTER,
@@ -65,14 +69,50 @@ var app = {
           overlap: false,
           offsetTopBar: false,
           bgColor: 'black'
-        } );
+        } );*/
+
+        $$(document).ready(function () {
+    
+            $$('*').bind('mousemove keydown scroll', function () {
+            
+                clearTimeout(idleTimer);
+                        
+                if (idleState == true) { 
+                    
+                    // Reactivated event
+                    //$$("body").append("<p>Welcome Back.</p>");            
+                }
+                
+                idleState = false;
+                
+                idleTimer = setTimeout(function () { 
+                    
+                    AdMob.createBanner( {
+                      adId: admobid.banner,
+                      adSize: 'SMART_BANNER',
+                      position: AdMob.AD_POSITION.BOTTOM_CENTER,
+                      isTesting: true, // TODO: remove this line when release
+                      overlap: false,
+                      offsetTopBar: false,
+                      bgColor: 'black'
+                    } );
+                    
+                    // Idle Event
+                    //$$("body").append("<p>You've been idle for " + idleWait/1000 + " seconds.</p>");
+
+                    idleState = true; }, idleWait);
+            });
+            
+            $$("body").trigger("mousemove");
+        
+        });
        
-         this will load a full screen ad on startup
-        AdMob.prepareInterstitial({
+        // this will load a full screen ad on startup
+        /*AdMob.prepareInterstitial({
           adId: admobid.interstitial,
           isTesting: true, // TODO: remove this line when release
           autoShow: true
-        });
+        });*/
 
         app.receivedEvent('deviceready');
     },
